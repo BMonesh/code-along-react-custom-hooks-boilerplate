c74675aa9f16c0779df3b88f736d45914d5c6e95
BMonesh <monesh.b@kalvium.community>
2024-01-26 22:09:21 +0530

    adanga

---
+++ code-along/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import InputField from './Component/inputField'
+
+function App() {
+  return (
+    <>
+      <InputField />
+    </>
+  )
+}
+
+export default App
